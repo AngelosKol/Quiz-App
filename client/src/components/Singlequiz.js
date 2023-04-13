38f25443da8a2355f67d8a908f5884be470e3570
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-04-13 20:55:52 +0300

    Next question functionality

--- client/src/components/Singlequiz.js
+++ client/src/components/Singlequiz.js
@@ -4,14 +4,12 @@ class Singlequiz {
   constructor() {
     this.body = document.querySelector("body");
     this._questions = [];
-    this.getQuestions();
   }
 
   async getQuestions() {
     try {
       const response = await QuestionsApi.getQuestions();
       this._questions = response.data.data;
-      console.log(this._questions);
     } catch (error) {
       console.log(error);
     }
@@ -24,15 +22,35 @@ class Singlequiz {
       array[j] = temp;
     }
   }
-  createButton() {
-    const button = document.createElement("button");
-    button.classList.add("btn");
-    return button;
+
+  changeQuestion(question, answersArray) {
+    const heading = document.getElementById("question");
+    heading.innerHTML = `${question.question}`;
+    const choices = Array.from(document.getElementsByClassName("choice-text"));
+    choices.forEach((choice, i) => {
+      choice.innerText = `${answersArray[i]}`;
+      choice.addEventListener("click", () => {
+        if (choice.innerText === question.correctAnswer) {
+          // increment the counter for correct answers
+          correctAnswers++;
+          // add a class to highlight the correct answer
+          choice.classList.add("correct");
+        } else {
+          // add a class to highlight the incorrect answer
+          choice.classList.add("incorrect");
+        }
+        // disable all choices after an answer is selected
+        choices.forEach((choice) => {
+          choice.removeEventListener("click", () => {});
+          choice.classList.add("disabled");
+        });
+      });
+    });
   }
 
   async render() {
     await this.getQuestions();
-    const question = this._questions[Math.floor(Math.random() * 80)];
+    const question = this._questions[Math.floor(Math.random() * 380)];
     const answers = [question.correctAnswer].concat(question.incorrectAnswers);
     this.shuffleArray(answers);
     this.body.classList.add("flex-simple");
@@ -58,11 +76,20 @@ class Singlequiz {
   </div>
   <div class="btn-container">
   <button class="btn">Return</button>
-  <button class="btn">Next</button>
+  <button class="btn" id="next">Next</button>
   </div>
   
   </div>
    `;
+    const nextButton = document.querySelector("#next");
+    nextButton.addEventListener("click", () => {
+      const nextQuestion = this._questions[Math.floor(Math.random() * 380)];
+      const nextAnswers = [nextQuestion.correctAnswer].concat(
+        nextQuestion.incorrectAnswers
+      );
+      this.shuffleArray(nextAnswers);
+      this.changeQuestion(nextQuestion, nextAnswers);
+    });
   }
 }
 
