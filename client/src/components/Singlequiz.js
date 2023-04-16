8f5bddca69fa73e22f72d52c38c1517916944e2b
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-04-16 21:06:49 +0300

    replaced piece of code by using choicesHandler method in it's place

--- client/src/components/Singlequiz.js
+++ client/src/components/Singlequiz.js
@@ -40,40 +40,24 @@ class Singlequiz {
     this.div.innerHTML = "";
     this.header.style.display = "block";
   }
+  //Change question & answers on next button click
   nextButtonHandler() {
     const question = this._questions[Math.floor(Math.random() * 380)];
     const answers = [question.correctAnswer].concat(question.incorrectAnswers);
     this.shuffleArray(answers);
     this.changeQuestion(question, answers);
   }
+
   changeQuestion(question, answersArray) {
     const heading = document.getElementById("question");
     const choices = Array.from(document.getElementsByClassName("choice-text"));
-
-    // remove all event listeners from the choices
     choices.forEach((choice) => {
       choice.removeEventListener("click", () => {});
     });
 
     heading.innerHTML = `${question.question}`;
-
-    choices.forEach((choice, i) => {
-      choice.innerText = `${answersArray[i]}`;
-      choice.addEventListener("click", () => {
-        if (choice.innerText === question.correctAnswer) {
-          this.correctAnswers++;
-          choice.classList.add("correct");
-        } else {
-          choice.classList.add("incorrect");
-          choice.classList.add("disabled");
-        }
-        // disable all choices after an answer is selected
-        choices.forEach((choice) => {
-          choice.removeEventListener("click", () => {});
-        });
-      });
-    });
-
+    //choicesHanlder renders choices with answers & add correct/incorrect classes
+    this.choicesHandler(choices, question, answersArray);
     //remove all classes from the choices
     choices.forEach((choice) => {
       choice.classList.remove("correct");
@@ -81,10 +65,28 @@ class Singlequiz {
       choice.classList.remove("disabled");
     });
   }
+  choicesHandler(choices, question, answersArray) {
+    choices.forEach((choice, i) => {
+      choice.innerText = `${answersArray[i]}`;
+      choice.addEventListener("click", () => {
+        if (choice.innerText === question.correctAnswer) {
+          this.correctAnswers++;
+          console.log(this.correctAnswers);
+          choice.classList.add("correct");
+        } else {
+          choice.classList.add("incorrect");
+          choice.classList.add("disabled");
+        }
+        choices.forEach((choice) => {
+          choice.removeEventListener("click", () => {});
+        });
+      });
+    });
+  }
 
   async render() {
     await this.startQuiz();
-    const question = this._questions[Math.floor(Math.random() * 380)];
+    const question = this._questions[Math.floor(Math.random() * 500)];
     const answers = [question.correctAnswer].concat(question.incorrectAnswers);
     this.shuffleArray(answers);
     this.header.style.display = "none";
@@ -94,19 +96,19 @@ class Singlequiz {
     <h2 id="question">${question.question}</h2>
     <div class="choice-container">
       <p class="choice-prefix">A</p>
-      <p class="choice-text" data-number="1">${answers[0]}</p>
+      <p class="choice-text" data-number="1"></p>
     </div>
     <div class="choice-container">
       <p class="choice-prefix">B</p>
-      <p class="choice-text" data-number="2">${answers[1]}</p>
+      <p class="choice-text" data-number="2"></p>
     </div>
     <div class="choice-container">
       <p class="choice-prefix">C</p>
-      <p class="choice-text" data-number="3">${answers[2]}</p>
+      <p class="choice-text" data-number="3"></p>
     </div>
     <div class="choice-container">
       <p class="choice-prefix">D</p>
-      <p class="choice-text" data-number="4">${answers[3]}</p>
+      <p class="choice-text" data-number="4"></p>
     </div>
   </div>
   <div class="btn-container">
@@ -120,39 +122,8 @@ class Singlequiz {
 
     this._nextButton = document.querySelector("#next");
     const choices = Array.from(document.getElementsByClassName("choice-text"));
-    this.choicesHandler(choices, question);
+    this.choicesHandler(choices, question, answers);
     this.addEventListeners();
-    // const choices = Array.from(document.getElementsByClassName("choice-text"));
-    // choices.forEach((choice) => {
-    //   choice.addEventListener("click", () => {
-    //     if (choice.innerText === question.correctAnswer) {
-    //       this.correctAnswers++;
-    //       choice.classList.add("correct");
-    //     } else {
-    //       choice.classList.add("incorrect");
-    //       choice.classList.add("disabled");
-    //     }
-    //     choices.forEach((choice) => {
-    //       choice.removeEventListener("click", () => {});
-    //     });
-    //   });
-    // });
-  }
-  choicesHandler(choices, question) {
-    choices.forEach((choice) => {
-      choice.addEventListener("click", () => {
-        if (choice.innerText === question.correctAnswer) {
-          this.correctAnswers++;
-          choice.classList.add("correct");
-        } else {
-          choice.classList.add("incorrect");
-          choice.classList.add("disabled");
-        }
-        choices.forEach((choice) => {
-          choice.removeEventListener("click", () => {});
-        });
-      });
-    });
   }
 }
 
