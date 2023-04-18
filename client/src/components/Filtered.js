067763f5888f2d5e1151f1f2271f1958243a16b9
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-04-18 17:00:36 +0300

    filter multiple questions with the answers

--- client/src/components/Filtered.js
+++ client/src/components/Filtered.js
@@ -6,6 +6,7 @@ class Filtered {
     this.div = document.querySelector(".container");
     this._questions = [];
     this._categories = [];
+    this.singlequiz = new Singlequiz();
   }
 
   addEventListeners() {
@@ -24,6 +25,24 @@ class Filtered {
     }
   }
 
+  choicesHandler() {
+    choices.forEach((choice, i) => {
+      choice.addEventListener("click", () => {
+        if (choice.innerText === question.correctAnswer) {
+          this.correctAnswers++;
+          console.log(this.correctAnswers);
+          choice.classList.add("correct");
+        } else {
+          choice.classList.add("incorrect");
+          choice.classList.add("disabled");
+        }
+        choices.forEach((choice) => {
+          choice.removeEventListener("click", () => {});
+        });
+      });
+    });
+  }
+
   //Renders the first five questions that match the filter criteria
 
   async renderQuestions() {
@@ -32,24 +51,51 @@ class Filtered {
     const response = await QuestionsApi.getQuestions(category, difficulty);
     this._questions = response;
     // Filter questions based on selected category and difficulty
-    const filteredQuestions = this._questions.filter((question) => {
-      return (
-        (!category || question.category === category) &&
-        (!difficulty || question.difficulty === difficulty)
-      );
-    });
-    console.log(filteredQuestions);
+    const filteredQuestions = this._questions
+      .filter((question) => {
+        return (
+          (!category || question.category === category) &&
+          (!difficulty || question.difficulty === difficulty)
+        );
+      })
+      .slice(0, 5);
     // Render the first five questions that match the filter criteria
     let html = "";
-    for (let i = 0; i < filteredQuestions.length && i < 5; i++) {
+    this.div.classList.add("flex-simple");
+
+    filteredQuestions.forEach((question, i) => {
+      const answers = [question.correctAnswer].concat(
+        question.incorrectAnswers
+      );
+      console.log(answers);
+
+      this.singlequiz.shuffleArray(answers);
+      console.log(answers);
       html += `
-      <div>
-        <h2>${filteredQuestions[i].question}</h2>
-        <p>Category: ${filteredQuestions[i].category}</p>
-        <p>Difficulty: ${filteredQuestions[i].difficulty}</p>
-      </div>
-    `;
-    }
+        <div class="quiz-container">
+          <h2 id="question">${question.question}</h2>
+          <p>Category: ${question.category}</p>
+          <p>Difficulty: ${question.difficulty}</p>
+          <div class="choice-container">
+            <p class="choice-prefix">A</p>
+            <p class="choice-text" data-number="1">${answers[0]}</p>
+          </div>
+          <div class="choice-container">
+            <p class="choice-prefix">B</p>
+            <p class="choice-text" data-number="2">${answers[1]}</p>
+          </div>
+          <div class="choice-container">
+            <p class="choice-prefix">C</p>
+            <p class="choice-text" data-number="3">${answers[2]}</p>
+          </div>
+          <div class="choice-container">
+            <p class="choice-prefix">D</p>
+            <p class="choice-text" data-number="4">${answers[3]}</p>
+          </div>
+        </div>
+      `;
+    });
+
     this.div.innerHTML = html;
   }
 
