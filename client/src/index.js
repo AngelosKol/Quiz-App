210835eac25479c009cf72e4ae396009d5a4fc6e
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-05-05 18:11:56 +0300

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,5 @@ import "./css/style.css";
 import Singlequiz from "./components/Singlequiz";
 import Filtered from "./components/Filtered";
 
-const randomPlayBtn = document.getElementById("random-mode");
-const filterPlayBtn = document.getElementById("filter-mode");
-
-// const modal = new Modal();
-const singleQuiz = new Singlequiz();
-const filteredMode = new Filtered();
-
-randomPlayBtn.addEventListener("click", () => {
-  singleQuiz.render();
-});
-
-filterPlayBtn.addEventListener("click", () => {
-  filteredMode.renderForm();
-});
+new Singlequiz();
+new Filtered();
